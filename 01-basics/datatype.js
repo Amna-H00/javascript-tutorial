05767497768cc8a23efe7b96e29490a9714c56c7
Amoo🩵 <ahdev000@gmail.com>
2025-07-03 16:22:33 +0000

    save

---
+++ 01-basics/datatype.js
@@ -0,0 +1,4 @@
+"use strictly" // treat all js code as newer version 
+// alert (3+3) //we are using nodejs not browser
+console.log("Mahrosh kli")
+//for standard readability documentation use ecma script
