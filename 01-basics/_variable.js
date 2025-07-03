c180cccd70dc928c113148987ce1f8190c253e4f
Amoo🩵 <ahdev000@gmail.com>
2025-07-03 07:19:01 +0000

    save config

--- 01-basics/_variable.js
+++ 01-basics/_variable.js
@@ -1,2 +1,23 @@
 console.log("Mahrosh Kli")
 //concept of let ,const and var
+//let --> is block scoped , can be reassigned , cant redeclared
+
+//i.e.
+// let — Redeclaration not Allowed
+//  let age = 25;
+//  let age = 30;
+// ❌ SyntaxError: Identifier 'age' has already been declared
+
+//i.e. 
+// let — Reassignment Allowed
+// let y = 5;
+//  y = 15;          // ✅ Allowed
+//console.log(y);  // 15
+
+//const --> block scoped , redeclaration not allowed , cant reassigned
+//⚠️ const does not make objects/arrays immutable (unchanged), only the binding is constant. i.e. 
+// const arr = [1, 2];
+// arr.push(3);  // ✅ Allowed
+// arr = [4, 5]; // ❌ Error
+
+
