07278a4b54cbfc20096c36937f1fca314132ad5e
Amoo🩵 <ahdev000@gmail.com>
2025-07-03 16:50:37 +0000

    save cont

--- 01-basics/datatype.js
+++ 01-basics/datatype.js
@@ -1,4 +1,18 @@
 "use strictly" // treat all js code as newer version 
 // alert (3+3) //we are using nodejs not browser
 console.log("Mahrosh kli")
-//for standard readability documentation use ecma script
+//for standard readability documentation use ecma script (tc39.es ecmascript , mdn)
+//two types od DT--> primitive (stored directly in variable)
+// number = intergers
+//bigint = big numbers like 1234556788n
+//string = enclosed in ""
+//boolean =either true / false
+//undefined =  a variable declared but not assigned a value
+//null = no value (standalone)
+//symbol = in ES6
+
+//non-primitive --> directly stored in memory
+//object = colection of keys & values like: ({ name : "kli"})
+//array = ordered lists of values e.g. [1,"two" ,{}]
+//function : callable blocks of code
+//builtin object type = such as date ,err(standard err , type err , range err) and regexp(patterns for matching text)
