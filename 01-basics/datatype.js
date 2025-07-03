5541f2a66fd0267796c18fce2938488acb583e58
Amoo🩵 <ahdev000@gmail.com>
2025-07-03 17:17:29 +0000

    save

--- 01-basics/datatype.js
+++ 01-basics/datatype.js
@@ -1,7 +1,9 @@
 "use strictly" // treat all js code as newer version 
 // alert (3+3) //we are using nodejs not browser
+
 console.log("Mahrosh kli")
 //for standard readability documentation use ecma script (tc39.es ecmascript , mdn)
+
 //two types od DT--> primitive (stored directly in variable)
 // number = intergers
 //bigint = big numbers like 1234556788n
