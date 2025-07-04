a22015423f4ccec300c3bd1e8312330eec6a1e16
Amoo🩵 <ahdev000@gmail.com>
2025-07-04 06:00:21 +0000

    saveconfig

---
+++ 01-basics/3_conversionoperators.js
@@ -0,0 +1,10 @@
+//conversions
+
+let score = "33abc";
+console.log(typeof score) //string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber) // now it will be number  
+
+console.log(valueInNumber) //now it will be NaN
+//this collectively called conversion.
