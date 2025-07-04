8f8c66dba88a0a9099a3eda6e9e91c6964c06f69
Amoo🩵 <ahdev000@gmail.com>
2025-07-04 06:14:15 +0000

    savecont

--- 01-basics/3_conversionoperators.js
+++ 01-basics/3_conversionoperators.js
@@ -1,10 +1,24 @@
 //conversions
 
-let score = "33abc";
+let score = "32abc";
 console.log(typeof score) //string
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber) // now it will be number  
 
 console.log(valueInNumber) //now it will be NaN
-//this collectively called conversion.
+//this collectively called conversion. 
+
+//conclusion:
+//"33" = 33
+//"33abc" = NaN
+//true = 1 , false = 0
+
+let isLoggedIn = "kli"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) //output:true
+
+//conclusion:
+//1 => true , 0 => false
+//"" => false
+//"kli" => true
