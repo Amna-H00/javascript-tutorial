c3763f197a573a00672f654934ff47f3a89712c0
Amoo🩵 <ahdev000@gmail.com>
2025-07-21 18:06:56 +0000

    save

--- 01-basics/8_date,time.js
+++ 01-basics/8_date,time.js
@@ -1,4 +1,17 @@
 //++++++++++++++++dates+++++++++++++++++//
 //date is an obj
+//1
+let myDate = new Date();
+console.log(myDate)
+console.log(myDate.toLocaleString())
+console.log(myDate.toString())
+console.log(typeof myDate)
+
+//2
 const hundreds = new Date(Date.UTC(2025,20,5,12,90,56));
 console.log(hundreds.toLocaleString('ko-KR' , {timeZone:'UTC'}))
+
+//3
+
+let mycreatedDate = new Date(2025 , 6, 21);
+console.log(mycreatedDate.toDateString());
