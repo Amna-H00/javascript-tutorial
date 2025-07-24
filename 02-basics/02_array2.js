6ed87ec707f7d1418c0169f264424a08c2859037
Amoo🩵 <ahdev000@gmail.com>
2025-07-24 18:41:02 +0000

    save

--- 02-basics/02_array2.js
+++ 02-basics/02_array2.js
@@ -6,10 +6,10 @@ const dchero = ['mahtarj' , 'bangu','banglu']
 //marvelhero.concat(dchero)
 //console.log(marvelhero)
 
-const allnewheros = [...marvelhero, ...dchero]
-console.log(allnewheros) //spread operator
+// const allnewheros = [...marvelhero, ...dchero]
+// console.log(allnewheros) //spread operator
 
 
-const anotherArray = [1,2,3,[2,3,4,[8,9,0]],[2,2],[3,4,5]]
-const anotherarray = anotherArray.flat(infinity);  //Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
-console.log(anotherarray)
+const anotherArray = [1,2,3 , [4,5,6],[7,8,9 , [10,11]]]
+const realarray = anotherArray.flat();  
+console.log(realarray);
