23d490769ec34cfb3587a1a4a91dc523773412d4
Amoo🩵 <ahdev000@gmail.com>
2025-07-24 16:35:21 +0000

    save cont

--- 02-basics/01_arrays.js
+++ 02-basics/01_arrays.js
@@ -43,9 +43,63 @@ console.log(Array.from([1, 2, 3], (x) => x + x)); //use of arrow function
 
 //  Methods Of Array : 
 //1- Mutators (which changes array)
+
 //(a) - push(): adds to end , returns new length.
-// const array = [1,2,3];
-  const arr = array.push(4,5)
-console.log(arr)
+const array = [1,2,3];
+  // const arr = array.push(4,5)
+// console.log(arr)
 console.log(array);
+
 //b  - pop() : removes last element
+console.log(array.pop())
+console.log(array)
+
+//c - unshift() : adds element to start
+//d - shift() : removes first elemnt 
+//e - splice() : removes n items optionally and insert items 
+
+let arr = ['a','b','c','d']
+console.log(arr.splice(1,2,'x','y'))
+console.log(arr)
+
+//f - reverse() : reverses the element
+console.log(arr.reverse())
+
+//// sort() : sorts the array in place,(arranges elements)
+let arra = [3,4,5,2,1]
+console.log(arra.sort())
+
+
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&//
+
+//accessor and utility methods of array which dont change i.e.
+//  concat() //--> combines array
+//   slice() // --> gives a copy from start up to end (end not including)
+//  join() : Concatenates array elements into a string, separated by the specified string (default is comma).
+//   toString() : Converts the array to a string—same as join(',').
+//   toLocaleString()--> localizes each element using each element’s own toLocaleString() method.
+//    includes() //--> returns true if contains otherwise false
+//    indexOf() //-> Returns the first index at which the value is found, or -1 if not found.
+//    lastIndexOf() //--> Returns the last index at which the value appears, or -1.
+//    flat() //
+
+   let sli = [1,2,5,6]
+   console.log(sli.slice(0,3));
+   console.log(sli.join('_'));
+
+   const hundreds = new Date(Date.UTC(2025,20,5,12,90,56));
+console.log(hundreds.toLocaleString('ko-KR' , {timeZone:'UTC'}))
+
+let mycreatedDate1 = new Date(2025 , 7, 21);
+// console.log(mycreatedDate.toDateString());
+ let mycreatedDate = new Date(2025 , 0 , 21 , 5 ,3);
+console.log(mycreatedDate.toLocaleString());
+
+
+iteration & Transformation
+forEach(), map(), filter(), reduce(), reduceRight()
+
+
+
+
