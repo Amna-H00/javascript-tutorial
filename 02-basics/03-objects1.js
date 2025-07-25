f31e7e63547453517e21ead60db43ec29c6b2282
Amoo🩵 <ahdev000@gmail.com>
2025-07-25 16:54:24 +0000

    save

---
+++ 02-basics/03-objects1.js
@@ -0,0 +1,11 @@
+// 1- Object Literals: (most common)
+
+let user = {
+      name : "Alice",
+      "full name" : "Alice choudhary",
+      age : 21,
+      LoggedIn : false
+     
+}
+console.log(user["full name"])
+console.log(user["name"])
