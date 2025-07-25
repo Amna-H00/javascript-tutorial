449b35de00e1120f97e1aa6427111e077c40443f
Amoo🩵 <ahdev000@gmail.com>
2025-07-25 22:21:34 +0000

    save integr

--- 02-basics/04-objects2.js
+++ 02-basics/04-objects2.js
@@ -21,4 +21,53 @@ console.log(Object.assign({} ,o1,o2,o3)); //Merging objects with same properties
 
 const o = {a:1};
 const O = {[Symbol("foo")]:2};
-console.log(Object.assign({},O,))
+console.log(Object.assign({},O,o))
+console.log(Object.getOwnPropertySymbols(O));//Copying symbol-typed properties
+
+const v1 = "abc";
+const v2 = true;
+const v3 = 10;
+const v4 = Symbol("liber");
+console.log(Object.assign({} , v1 , null , v2 , undefined, v3 , v4)) //Primitives will be wrapped to objects
+
+
+//2- Object Entries:
+const object = {
+    a : "some str",
+    b : 45,
+    gender : "female"
+}
+console.log(Object.keys(object));
+console.log(Object.entries(object));
+
+//3- Object.fromentries():
+
+const map = new Map([
+    ["liber" , "bar"],
+    ["baz",42]
+]);
+console.log(Object.fromEntries(map))//Converting a Map to an Object
+
+const arr = [
+  ["0", "a"],
+  ["1", "b"],
+  ["2", "c"],
+];
+console.log(Object.fromEntries(arr)); //Converting an Array to an Object
+
+const regularuser = {
+    email : "gona@gmai.com",
+    fullname: {
+        userfullname : {
+            fisrtname : "alira",
+            lastname : "alyana"
+        }
+    }
+}
+console.log(regularuser.fullname.userfullname.fisrtname);//object wraped in obj
+
+
+const objectu = {...obj1 , ...anothersource};
+console.log(objectu) //spread operator for combining values
+ 
+console.log(regularuser.hasOwnProperty('fullname'));
