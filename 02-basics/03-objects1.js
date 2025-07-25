6f844f10fbe929cc1b25de1d73f045811d688d25
Amoo🩵 <ahdev000@gmail.com>
2025-07-25 17:18:51 +0000

    save integ

--- 02-basics/03-objects1.js
+++ 02-basics/03-objects1.js
@@ -1,11 +1,32 @@
+//singleton 
+//object .create
+
 // 1- Object Literals: (most common)
 
+const mysym = Symbol("key1"); //useof symbol
 let user = {
       name : "Alice",
       "full name" : "Alice choudhary",
       age : 21,
+      email : "coder@gmail.com",
+      [mysym] : "mykey1",
       LoggedIn : false
      
 }
 console.log(user["full name"])
 console.log(user["name"])
+console.log(user.age);
+console.log(user.mysym)
+console.log(typeof  user.mysym)
+console.log(user[mysym])
+
+//for changing & locking obj value:
+user.email ="codermicrosoft@gmail.com"
+console.log(user.email)
+
+Object.freeze(user);
+user.email = "codergoogle.com"
+console.log(user)
+
+
+ 
