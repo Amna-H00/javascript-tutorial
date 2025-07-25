ad79a8a146e110802f67639b8a2548d141e424e6
Amoo🩵 <ahdev000@gmail.com>
2025-07-25 19:03:20 +0000

    save integral

---
+++ 02-basics/04-objects2.js
@@ -0,0 +1,9 @@
+//singleton objects
+ 
+//1 - Assign object: 
+
+const source = {a:1 , b:2};
+const anothersource = {a:"mahrosh" , b:"8"}
+
+const returnedsource = Object.assign(source,anothersource);
+console.log(returnedsource);
