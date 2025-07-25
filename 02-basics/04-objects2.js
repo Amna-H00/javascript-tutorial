abc8f6ffe07cb94dfffc3bfc0754d7feb2d0b917
Amoo🩵 <ahdev000@gmail.com>
2025-07-25 21:43:43 +0000

    save

--- 02-basics/04-objects2.js
+++ 02-basics/04-objects2.js
@@ -3,7 +3,22 @@
 //1 - Assign object: 
 
 const source = {a:1 , b:2};
-const anothersource = {a:"mahrosh" , b:"8"}
-
+const anothersource = {c:"mahrosh" , d:"8"}
 const returnedsource = Object.assign(source,anothersource);
-console.log(returnedsource);
+console.log(returnedsource); //merging objects
+
+console.log(Object.assign({},(source))) //cloning
+
+const obj1 = {a:0,b:{c:0}};
+const obj2 = Object.assign({} , obj1)
+console.log(obj2) //deep cloning
+
+const o1 = {a:1 , b:2 , c: 1};
+const o2 = { b:7 , c: 2};
+const o3 = {c:8 };
+console.log(Object.assign({} ,o1,o2,o3)); //Merging objects with same properties
+
+
+const o = {a:1};
+const O = {[Symbol("foo")]:2};
+console.log(Object.assign({},O,))
