8c553bb80fbb9b7471027be6b375f9311f49bf2c
Amoo🩵 <ahdev000@gmail.com>
2025-07-30 06:56:20 +0000

    function general

---
+++ 03-basics/01-functions.js
@@ -0,0 +1,11 @@
+//Functions 
+
+function sayMyName() {
+    console.log( 'A' );
+    console.log( 'm' );
+    console.log( 'n' );
+    console.log( 'o' );
+    console.log( 'o' );
+}
+sayMyName(); // General Form of functions
+
