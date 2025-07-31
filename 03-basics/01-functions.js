5edd02122e2f621a6152727d267ca65a84babaa8
Amoo🩵 <ahdev000@gmail.com>
2025-07-31 05:46:14 +0000

    functions in arr

--- 03-basics/01-functions.js
+++ 03-basics/01-functions.js
@@ -32,5 +32,39 @@ function LoginUserMessage(username = "zoam") {
     return `${username} just logged in`
     
 }
-console.log(LoginUserMessage());
+//console.log(LoginUserMessage());
+
+//type 4 function for list of prices
+function calculateCartPrice(...num1) { //rest operator
+    return num1;
+}
+console.log(calculateCartPrice(200,300,400))
+
+
+function calculateCartPrice(val1,val2,...num1) {
+    return num1;
+}
+//console.log(calculateCartPrice(200,300,400,2000,600))
+
+//type 5 functions in objects:
+const user = {
+    username : "vakom",
+    price : 200
+}
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+handleObject({
+    username:"vanum",
+    price : 300
+})//we can also use direct obj
+
+//type 6 arrays in function:
+const myNewArray = [200,400,5600,900]
+function returnArray(getArray) {
+    return getArray[0]
+}
+console.log(returnArray(myNewArray));
+
 
