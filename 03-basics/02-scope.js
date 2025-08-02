4dcee0c7940f482db02f25b068cc9d39af51e39e
Amoo🩵 <ahdev000@gmail.com>
2025-08-02 17:39:28 +0000

    func js

--- 03-basics/02-scope.js
+++ 03-basics/02-scope.js
@@ -2,3 +2,27 @@
 //var = Globally scoped
 //let,var,const = Function scoped
 //let , const = Block scoped
+//nested scope : which contains when a scope is declared inside another. function inside function
+
+function one (){
+    const username = "Aliana";
+    function two(){
+        const website = "W3.SCH#10"
+        console.log(username)
+    }
+    //console.log(website);
+    two()
+}
+one(); //nested func
+
+
+function addone(num){
+    return num + 1
+}
+addone(5);//type 1 to declare function
+
+const addtwo = function (num) {
+    return num + 2
+}
+addtwo();//type 2 to declare
+
