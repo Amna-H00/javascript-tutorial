19fe67afb72b88eaba44b73b40201ab049147607
Amoo🩵 <ahdev000@gmail.com>
2025-08-04 05:55:51 +0000

    IIFE

--- 03-basics/04-life.js
+++ 03-basics/04-life.js
@@ -1,1 +1,19 @@
-//Imediately invoked Function (IIFE) : 
+//Imediately invoked Function (IIFE) : to avoid from global scope pollution use IIFE () () like this
+//1-
+(function chai() {
+    console.log(`DB IS CONNECTED`)
+})(); //we use ; in IIFE sometime to Execute func
+//2-
+(function chai() {
+    console.log(`DB IS CONNECTED TWO`)
+})() //IIFE
+
+//3 
+(  () => {
+    console.log(`DB IS CONNECTED THREE`)
+})(); //replacing func with arrow func
+
+//4 
+(  (name) => {
+    console.log(`DB IS CONNECTED 4 ${name}`)
+})('OLIvia') //chnging arguments
