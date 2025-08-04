1366f6948795a81c1e28d0faaf8029c0d11a5316
Amoo🩵 <ahdev000@gmail.com>
2025-08-04 06:04:33 +0000

    2 IIFE

--- 03-basics/04-life.js
+++ 03-basics/04-life.js
@@ -2,11 +2,11 @@
 //1-
 (function chai() {
     console.log(`DB IS CONNECTED`)
-})(); //we use ; in IIFE sometime to Execute func
+})(); //we use ; in IIFE sometime to Execute func . named iIFE
 //2-
 (function chai() {
     console.log(`DB IS CONNECTED TWO`)
-})() //IIFE
+})(); //IIFE
 
 //3 
 (  () => {
@@ -16,4 +16,7 @@
 //4 
 (  (name) => {
     console.log(`DB IS CONNECTED 4 ${name}`)
-})('OLIvia') //chnging arguments
+})('OLIvia'); 
+
+//++++++++++++++++++++++++++important+++++++++++++++++
+//for writing 2 or more IIFE use semicolon at the end
