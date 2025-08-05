d0f6d190bc60dc87244fdb3d94f73362690cae0b
Amoo🩵 <ahdev000@gmail.com>
2025-08-05 05:08:24 +0000

    CF

---
+++ 04-control-flow/one.js
@@ -0,0 +1,10 @@
+//OPERATORS:
+//< , >, <= , >= , == , ===(strict equal to chcks datatype), != , 
+
+const isUserLoggedIn = true;
+const temperature = 41;
+if (temperature < 50 ) {
+    console.log("less than 50")
+}else{
+    console.log("False condition")
+}
