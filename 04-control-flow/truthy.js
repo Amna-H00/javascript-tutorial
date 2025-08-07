bda298fabc0787d55a477d06d477dce9be842b13
Amoo🩵 <ahdev000@gmail.com>
2025-08-07 17:02:24 +0000

    T/F val

--- 04-control-flow/truthy.js
+++ 04-control-flow/truthy.js
@@ -1,1 +1,14 @@
  //a truthy value is any value that evaluates to true when used in a Boolean context
+ 
+ const userEmail = " ";
+ if (userEmail) {
+    console.log("Got Ur Email")
+ } else {
+    console.log("dont Hve User Email")
+ }
+
+ //falsy values:
+ //false , 0 , -0 , BigInt 0n , null , undefined , NaN , ""
+
+ //truthy values:
+//  "0" , 'false' , " " , [] , {} , function () {}
