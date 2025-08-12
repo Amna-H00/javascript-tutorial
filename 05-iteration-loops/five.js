2680363753d31e76a448aed55506ab31055ed623
Amoo🩵 <ahdev000@gmail.com>
2025-08-12 17:28:23 +0000

    filter map

--- 05-iteration-loops/five.js
+++ 05-iteration-loops/five.js
@@ -2,6 +2,42 @@
 
 const programming = ["js" , "rb" , "py" , "thon"]
 
-programming.forEach(function (val) {
-    console.log(val)
-})
+//programming.forEach(function (val) {
+   // console.log(val)
+//})
+
+programming.forEach((item) => {
+    console.log(item)
+}) //for arrow func
+
+function printMe(item) {
+    console.log(item)
+}
+programming.forEach(printMe)//callback func
+
+programming.forEach((item,index,arr) =>{
+    console.log(item,index,arr)
+})//foreach loop also hve parameter like item,index,arr
+
+
+const myCoding = [
+    {
+        languageName: "Python",
+        languageFileName : "javascript"
+    },
+    {
+        languageName: "java",
+        languageFileName : "javascript"
+    },
+    {
+        languageName: "Ruby",
+        languageFileName : "RB"
+    }
+   
+] //iteration on array inside object 
+
+myCoding.forEach((item) => {
+    console.log(item.languageName)
+})//calling arr of obj
+
+
